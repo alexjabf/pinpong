6e6156fb814de199fe4ea7eac5ff64baf3281ae3
Alejandro Ballesteros <alexjabf@hotmail.com>
2017-05-17 17:17:54 -0500

    Big fixded in log games table

--- db/migrate/20170517203018_create_log_games.rb
+++ db/migrate/20170517203018_create_log_games.rb
@@ -2,7 +2,7 @@ class CreateLogGames < ActiveRecord::Migration
   def change
     create_table :log_games do |t|
       t.datetime :date_played
-      t.references :user, index: true, foreign_key: true
+      t.references :owner, index: true, foreign_key: true
       t.references :opponent, index: true, foreign_key: true
       t.integer :your_score
       t.integer :opponent_score
--- db/migrate/20170517221222_add_opponent_id_to_log_games.rb
+++
@@ -1,5 +0,0 @@
-class AddOpponentIdToLogGames < ActiveRecord::Migration
-  def change
-    add_reference :log_games, :opponent, index: true, foreign_key: true
-  end
-end
